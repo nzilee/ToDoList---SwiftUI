3357f955c832f83c128a33d25f68efbae5ffd683
Nikola Zivkovic <nzivkovic7@gmail.com>
2023-08-12 18:41:50 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Nikola Zivkovic on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
